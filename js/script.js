6f88d5ef376858c9cf72a692d0083fcbacb54e2e
Brent Guffens <brentguf@users.noreply.github.com>
2017-06-04 13:21:11 +0200

    jquery stat count up

---
+++ js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  $('.stat-figure').each(function() {
+    var $this = $(this),
+        countTo = $this.attr('data-count');
+    
+    $({ countNum: $this.text()}).animate({
+      countNum: countTo
+    },
+
+    {
+
+      duration: 1200,
+      easing:'linear',
+      step: function() {
+        $this.text(Math.floor(this.countNum));
+      },
+      complete: function() {
+        $this.text(this.countNum);
+        //alert('finished');
+      }
+
+    }); 
+
+  });
+});
+
