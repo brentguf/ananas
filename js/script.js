f1a22776b3ab4a44f73f30c885c8bd018ed31726
Brent Guffens <brentguf@users.noreply.github.com>
2017-06-05 17:09:00 +0200

    waypoint countup trigger

--- js/script.js
+++ js/script.js
@@ -2,31 +2,42 @@
 
 $(document).ready(function() {
 
+  // Scroll reveal
   window.sr = ScrollReveal();
   sr.reveal('section');
 
-  $('.stat-figure').each(function() {
-    var $this = $(this),
-        countTo = $this.attr('data-count');
-    
-    $({ countNum: $this.text()}).animate({
-      countNum: countTo
-    },
+  // Waypoint
 
-    {
+  var waypoint = new Waypoint({
+    element: document.getElementById('countup-trigger'),
+    handler: function() {
+      // Stat count up
+      $('.stat-figure').each(function() {
+        var $this = $(this),
+            countTo = $this.attr('data-count');
+        
+        $({ countNum: $this.text()}).animate({
+          countNum: countTo
+        },
 
-      duration: 1200,
-      easing:'linear',
-      step: function() {
-        $this.text(Math.floor(this.countNum));
-      },
-      complete: function() {
-        $this.text(this.countNum);
-        //alert('finished');
-      }
+        {
 
-    }); 
+          duration: 1200,
+          easing:'linear',
+          step: function() {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function() {
+            $this.text(this.countNum);
+            //alert('finished');
+          }
 
-  });
+        }); 
+
+      });
+    }
+  })
+
+  
 });
 
